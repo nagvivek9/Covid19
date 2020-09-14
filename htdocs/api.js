d20da3f858d794e6d1cad636ffb9f01186d86b52
Nagendra Manthina <nagvivek9@gmail.com>
2020-09-14 18:51:34 +0530

    APIs to CRUD operations

--- htdocs/api.js
+++ htdocs/api.js
@@ -41,10 +41,110 @@ R.post('/login',function(req,res,cb) {
  
 });
 function auth_token(req,res,cb) {
+ var token=req.USERDATA.P('_T');
+ if(!token) return res.json({status:'auth'});
+ mysql.Q('SELECT 1 FROM home_token WHERE token=:token LIMIT 1',
+  {token},function(e,r){
+   if(e) return cb(e);
+   if(!r.length) return res.json({status:'auth'});
+   cb();
+  });
  cb();
 }
+const countries={
+ 'INDIA': {
+  'Telangana': {
+   positive: 100,
+   totaltests: 200000
+  },
+  'Andhrapradesh': {
+   positive: 200,
+   totaltests: 300000
+  }
+ },
+ 'GERMANY': {
+  'Hamburg': {
+   positive: 50,
+   totaltests: 300000
+  },
+  'Hesse': {
+   positive: 500,
+   totaltests: 100000
+  }
+ },
+ 'SPAIN': {
+  'Madrid': {
+   positive: 200,
+   totaltests: 300000
+  },
+  'Málaga': {
+   positive: 90,
+   totaltests: 500000
+  }
+  
+ },
+ 'SRILANKA': {
+  'Anuradhapura': {
+   positive: 200,
+   totaltests: 300000
+  },
+  'Badulla': {
+   positive: 800,
+   totaltests: 50000
+  }
+  
+ }};
 R.all('/countries',auth_token,function(req,res,cb){
-
+ res.json({status:'ok', list:Object.keys(countries)});
 });
 
+R.all('/states',auth_token,function(req,res,cb){
+ var country=req.USERDATA.P('country');
+ if(!country) return res.json({status:'data'});
+ if(!countries[country]) return res.json({status:'notfound'});
+ res.json({status:'ok', list:countries[country]});
+});
+
+R.post('/user/create', function(req,res,cb){
+ var fn=req.USERDATA.P('firstname');
+ var ln=req.USERDATA.P('lastname');
+ var un=req.USERDATA.P('username');
+ var pwd=req.USERDATA.P('password');
+ mysql.Q(`INSERT INTO home_user(firstname,lastname,username,password,date_created) 
+         VALUES(:fn,:ln,ün,:pwd,now())`,
+ {fn,ln,un,pwd},function(e,r){
+  if(e) return cb(e);
+  res.json({status:'ok'});
+ });
+});
+R.get('/user/:uid', function(req,res,cb){
+ var uid=req.USERDATA.P('uid');
+ mysql.Q(`SELECT firstname,lastname,username,password FROM home_user WHERE id=:uid`,
+ {uid},function(e,r){
+  if(e) return cb(e);
+  if(!r.length) return res.json({status:'notfound'});
+  res.json({status:'ok',user:r[0]});
+ });
+});
+R.put('/user/update', function(req,res,cb){
+ var fn=req.USERDATA.P('firstname');
+ var ln=req.USERDATA.P('lastname');
+ var un=req.USERDATA.P('username');
+ var pwd=req.USERDATA.P('password');
+ mysql.Q(`UPDATE home_user SET firstname=COALESCE(:fn,firstname),lastname=COALESCE(:ln,lastname),
+ username=COALESCE(:un,username),password=COALESCE(:pwd,password))`,
+ {fn,ln,un,pwd},function(e,r){
+  if(e) return cb(e);
+  res.json({status:'ok'});
+ });
+});
+
+R.delete('/user/:uid', function(req,res,cb){
+ var uid=req.USERDATA.P('uid');
+ mysql.Q(`DELETE FROM home_user WHERE id=:uid`,
+ {uid},function(e,r){
+  if(e) return cb(e);
+  res.json({status:'ok'});
+ });
+});
 module.exports = R;
