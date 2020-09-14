9fae971fdf4687602e0f705a31e9e81afea3835c
Nagendra Manthina <nagvivek9@gmail.com>
2020-09-14 16:06:42 +0530

    function to show/hide screen

--- htdocs/index.js
+++ htdocs/index.js
@@ -1,3 +1,9 @@
+const screens=['loginscreen','mainscreen'];
+function showScreen(id) {
+ screens.forEach(s=>document.getElementById(s).style.display='none');
+ document.getElementById(id).style.display='block';
+}
+
 function pageload() {
  console.log('Pageloaded');
 }
